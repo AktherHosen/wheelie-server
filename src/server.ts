29d72ec600cc026d9dcd4d37b55ae0a5ab1f6464
aktherhosen <dev.aktherhosen@gmail.com>
2025-11-12 01:11:27 +0600

    feat: eslint setup completed

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,58 @@
-import {Server} from "http"
-import express, { Request, Response } from "express"
+import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 
 let server: Server;
 
-
 const startServer = async () => {
-    try {
-        await mongoose.connect("mongodb+srv://wheelie:tpp7iIUBT09GCbH8@cluster0.bmhyihx.mongodb.net/wheelie?appName=Cluster0")
+  try {
+    await mongoose.connect(
+      "mongodb+srv://wheelie:tpp7iIUBT09GCbH8@cluster0.bmhyihx.mongodb.net/wheelie?appName=Cluster0"
+    );
 
-    console.log("Connect to DB!")
-    server = app.listen(5001, ()=> {
-        console.log(`Server is listening to port 5001`)
-    })
-    } catch (error) {
-        console.log(error)
-    }
-}
+    console.log("Connect to DB!");
+    server = app.listen(5001, () => {
+      console.log(`Server is listening to port 5001`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-startServer()
+startServer();
 
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received.. Server is shutting down!");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
 
+// Unhandled rejection error
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected.. Server is shutting down!", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+Promise.reject(new Error("I forgot to catch this promise"));
+
+// Unhandled exception error
+process.on("uncaughtException", (err) => {
+  console.log("Unhandled Exception detected.. Server is shutting down!", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// throw new Error("I forgot to handle this local error")
